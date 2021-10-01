96841e6cee98fc4b2310d6bbaac72ff590e5b551
SM Boolean <samuelemadrigali1991@gmail.com>
2021-10-01 12:02:41 +0200

    Struttura base componenti

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Characters/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Characters from './components/Characters.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Characters
   }
 }
 </script>
