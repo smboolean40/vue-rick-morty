9eaf52c5f4f734e6ce3967f21313231116d2bf6e
SM Boolean <samuelemadrigali1991@gmail.com>
2021-10-01 12:50:37 +0200

    Aggiunti personaggi

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,16 +1,33 @@
 <template>
-  <section class="characters">
-	<Character/>
+  <section class="characters container">
+	<div class="row">
+		<div class="col-6 col-md-4 col-lg-3 mb-3" v-for="(elm, index) in characters" :key="index">
+			<Character :info="elm"/>
+		</div>
+	</div>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
 import Character from './Character.vue';
 
 export default {
 	name: "Characters",
 	components: {
 		Character
+	},
+	data() {
+		return {
+			characters: []
+		}
+	},
+	created() {
+		axios
+			.get("https://api.sampleapis.com/rickandmorty/characters")
+			.then( (res) => {
+				this.characters = res.data;
+			});
 	}
 }
 </script>
